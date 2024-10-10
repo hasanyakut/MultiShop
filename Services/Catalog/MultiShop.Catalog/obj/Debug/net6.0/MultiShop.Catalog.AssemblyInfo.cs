7e731f310a40813fe8550b6b0ff2a619cf2c83df
Hasan Huseyin Yakut <hasanhuseyinyakut@gmail.com>
2024-10-10 03:36:15 +0300

    Catalog Settings

--- Services/Catalog/MultiShop.Catalog/obj/Debug/net6.0/MultiShop.Catalog.AssemblyInfo.cs
+++ Services/Catalog/MultiShop.Catalog/obj/Debug/net6.0/MultiShop.Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a81ef2dad1671797aa4b476576126688e1e4e82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bc045e4b5e1d63d12b5bb21f7bc5a437e862dc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
