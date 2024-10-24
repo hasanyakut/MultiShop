af11194518bdbb647ae7b26870d84d8f7e8449ce
Hasan Huseyin Yakut <hasanhuseyinyakut@gmail.com>
2024-10-25 00:21:22 +0300

    cargo Business Concretes

---
+++ Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoCompany.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+
+namespace MultiShop.Cargo.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CargoCompany : ControllerBase
+	{
+		private readonly ICargoCompanyService _cargoCompanyService;
+
+		public CargoCompany(ICargoCompanyService cargoCompanyService)
+		{
+			_cargoCompanyService = cargoCompanyService;
+		}
+		[HttpGet]
+		public IActionResult CargoCompanyList()
+		{
+			var values = _cargoCompanyService.TGetAll();
+			return Ok(values);
+		}
+
+		[HttpPost]
+		public IActionResult CreateCargoCompany()
+		{
+			return Ok("Kargo şirketi başarıyla oluşturuldu");
+		}
+
+		[HttpDelete]
+		public IActionResult RemoveCargoCompany(int id)
+		{
+			_cargoCompanyService.TDelete(id);
+			return Ok("Kargo şirketi başarıyla silindi.");
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetCargoCompanyById(int id)
+		{
+			var values = _cargoCompanyService.TGetById(id);
+			return Ok(values);
+		}
+
+		[HttpPut]
+		public IActionResult UpdateCargoCompany()
+		{
+			return Ok("kargo şirketi başarıyla güncellendi");
+		}
+	}
+}
