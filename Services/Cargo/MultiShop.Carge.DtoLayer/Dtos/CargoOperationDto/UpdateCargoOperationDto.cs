fed3a2193a16baabd474ae6c2c3fc6782e54437f
Hasan Huseyin Yakut <hasanhuseyinyakut@gmail.com>
2024-10-25 01:09:09 +0300

    Cargo Registration

--- Services/Cargo/MultiShop.Carge.DtoLayer/Dtos/CargoOperationDto/UpdateCargoOperationDto.cs
+++ Services/Cargo/MultiShop.Carge.DtoLayer/Dtos/CargoOperationDto/UpdateCargoOperationDto.cs
@@ -8,5 +8,9 @@ namespace MultiShop.Carge.DtoLayer.Dtos.CargoOperationDto
 {
 	public class UpdateCargoOperationDto
 	{
+		public int CargoOperationId { get; set; }
+		public string Barcode { get; set; }
+		public string Description { get; set; }
+		public DateTime OperationDate { get; set; }
 	}
 }
