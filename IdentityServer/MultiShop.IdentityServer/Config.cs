1af0ec01e53a307e72a19697b69c6a522a8ddd29
Hasan Huseyin Yakut <hasanhuseyinyakut@gmail.com>
2024-11-12 02:30:36 +0300

    Basket Service

--- IdentityServer/MultiShop.IdentityServer/Config.cs
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -19,6 +19,8 @@ namespace MultiShop.IdentityServer
             new ApiResource("ResourceOrder"){Scopes ={"OrderFullPermission"}},
 
             new ApiResource("ResourceCargo"){Scopes ={"CargoFullPermission"}},
+           
+            new ApiResource("ResourceBasket"){Scopes ={"BasketFullPermission"}},
 
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
@@ -37,6 +39,7 @@ namespace MultiShop.IdentityServer
             new ApiScope("DiscountFullPermission","Full authority for discount operations"),
             new ApiScope("OrderFullPermission","Full authority for order operations"),
             new ApiScope("CargoFullPermission","Full authority for cargo operations"),
+            new ApiScope("BasketFullPermission","Full authority for basket operations"),
             new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
 		};
 
@@ -67,7 +70,7 @@ namespace MultiShop.IdentityServer
                 AllowedGrantTypes=GrantTypes.ClientCredentials,
                 ClientSecrets={new Secret("multishopsecret".Sha256())},
                 AllowedScopes={ "CatalogFullPermission", "DiscountFullPermission", "OrderFullPermission",
-                    "CargoFullPermission",
+                    "CargoFullPermission","BasketFullPermission",
 				IdentityServerConstants.LocalApi.ScopeName,
                 IdentityServerConstants.StandardScopes.Email,
                 IdentityServerConstants.StandardScopes.OpenId,
